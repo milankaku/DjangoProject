2c4db7199c0c91fc2d3713e6a6c676a3ad862dc9
milankaku <milankaku1@gmail.com>
2017-02-15 00:47:19 -0600

    custom unit test error, add to html template to handle table unittests

--- djangoproject/functional_tests.py
+++ djangoproject/functional_tests.py
@@ -27,7 +27,7 @@ class NewUserTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == 'Buy some milk tomorrow' for row in rows))
+        self.assertTrue(any(row.text == 'Buy some milk tomorrow' for row in rows), "To do task not in table")
 
         self.fail('Add testing for input box to add more to do tasks')
 
