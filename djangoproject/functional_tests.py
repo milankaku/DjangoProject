3d4e9c28e7fbe144990b651afca33e8d50a8ae0c
milankaku <milankaku1@gmail.com>
2017-02-14 23:01:18 -0600

    tests now use unittests

--- djangoproject/functional_tests.py
+++ djangoproject/functional_tests.py
@@ -1,8 +1,20 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://127.0.0.1:8000/')
+class NewUserTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
 
-browser.quit()
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_create_todo_list_and_retrieve_later(self):
+        self.browser.get('http://127.0.0.1:8000/')
+
+        self.assertIn('To Do List', self.browser.title)
+        self.fail(self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main()
