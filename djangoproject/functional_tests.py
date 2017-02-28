4b13b5d97e81913f43b709e2161bdb1d0cf5e7ee
milankaku <milankaku1@gmail.com>
2017-02-28 14:02:32 -0600

    refactor FT to make the table check for a row dynamic and not hardcoded

--- djangoproject/functional_tests.py
+++ djangoproject/functional_tests.py
@@ -11,6 +11,11 @@ class NewUserTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_create_todo_list_and_retrieve_later(self):
         self.browser.get('http://127.0.0.1:8000/')
 
@@ -25,9 +30,14 @@ class NewUserTest(unittest.TestCase):
         inputarea.send_keys('Buy some milk tomorrow')
         inputarea.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1. Buy some milk tomorrow', [row.text for row in rows])
+        self.check_for_row_in_table('1: Buy some milk tomorrow')
+
+        inputarea = self.browser.find_element_by_id('id_new_task')
+        inputarea.send_keys('Buy some potatoes today')
+        inputarea.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_table('1: Buy some milk tomorrow')
+        self.check_for_row_in_table('2: Buy some potatoes today')
 
         self.fail('Add testing for input box to add more to do tasks')
 
